a32c653f7ae095a39ce60a0c73b44e56797cd7cf
Hua <ee358474@gmail.com>
2025-03-05 10:01:24 +0800

    Implement q_remove to remove the element in queue
    Change-Id: Ie0c2aedb534feced57ca7bf1c9f8ec7badaf9603

--- queue.c
+++ queue.c
@@ -101,13 +101,30 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    list_head *node = head->next;
+    element_t *str = list_entry(node, element_t, list);
+    bufsize = strlen(str->value) + 1;
+    strlcpy(sp, str->value, bufsize);
+    list_del(node);
+    return str;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    list_head *node = head->prev;
+    element_t *str = list_entry(node, element_t, list);
+    bufsize = strlen(str->value) + 1;
+    strlcpy(sp, str->value, bufsize);
+    list_del(node);
+
+    return str;
 }
 
 /* Return number of elements in queue */
